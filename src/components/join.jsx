8b6de24426bf960c767e5afe7d793b96fb4412ff
jiwon9412 <80965224+jiwon9412@users.noreply.github.com>
2023-01-16 02:21:07 +0900

    update join component

--- src/components/join.jsx
+++ src/components/join.jsx
@@ -1,15 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Join = () => {
+  const [enteredId, setEnteredId] = useState("");
+  const [enteredIdIsTouched, setEnteredIdIsTouched] = useState(false);
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredPasswordIsTouched, setEnteredPasswordIsTouched] =
+    useState(false);
+
+  const enteredIdIsValid = enteredId.length >= 6; //Id 8자리 이상인지 유효성검사
+  /**
+   *
+   * @param {*} password //입력한 password
+   * @returns boolean : password 유효성검사결과
+   */
+  const checkValidationPassword = (password) => {
+    let checkLength = false;
+    let checkIncludeSpecial = false;
+    if (password.length >= 8) {
+      checkLength = true;
+    }
+    const specialCharacterRegExp =
+      /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g; //특수문자 정규표현식
+    if (specialCharacterRegExp.test(password)) {
+      checkIncludeSpecial = true;
+    }
+
+    return checkLength && checkIncludeSpecial;
+  };
+
+  const enteredPasswordIsValid = checkValidationPassword(enteredPassword); //Password 유효성검사 (8자이상, 특수문자 포함)
+
+  const handleIdChange = (e) => {
+    setEnteredId(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setEnteredPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!enteredIdIsValid) {
+      alert("ID를 확인하세요.");
+      return;
+    }
+    if (!enteredPasswordIsValid) {
+      alert("Password를 확인하세요.");
+      return;
+    }
+
+    alert(`ID : ${enteredId} \n Password : ${enteredPassword}`);
+  };
+
+  const handleIdInputBlur = () => {
+    setEnteredIdIsTouched(true);
+  };
+
+  const handlePasswordInputBlur = () => {
+    setEnteredPasswordIsTouched(true);
+  };
+
   return (
     <JoinBox>
       <p>Your ID</p>
-      <input type='text' />
+      <input type='text' onChange={handleIdChange} onBlur={handleIdInputBlur} />
+      {!enteredIdIsValid && enteredIdIsTouched && (
+        <WarningText>ID는 6자리 이상만 가능합니다.</WarningText>
+      )}
       <p>Password</p>
-      <input type='text' />
+      <input
+        type='password'
+        onChange={handlePasswordChange}
+        onBlur={handlePasswordInputBlur}
+      />
+      {!enteredPasswordIsValid && enteredPasswordIsTouched && (
+        <WarningText>
+          Password는 8자이상이고 특수문자를 포함하여야합니다.
+        </WarningText>
+      )}
       <BottomBox>
-        <button>Submit</button>
+        <button onClick={handleSubmit}>Submit</button>
       </BottomBox>
     </JoinBox>
   );
@@ -21,7 +92,8 @@ const JoinBox = styled.div`
   display: flex;
   flex-direction: column;
 
-  width: 60%;
+  width: 40%;
+  min-width: 460px;
   padding: 10px 20px;
   background-color: #fff;
   border-radius: 10px;
@@ -46,6 +118,7 @@ const JoinBox = styled.div`
 const BottomBox = styled.div`
   display: flex;
   justify-content: flex-end;
+  margin-top: 10px;
 
   button {
     display: block;
@@ -63,4 +136,12 @@ const BottomBox = styled.div`
       font-weight: 600;
     }
   }
+`;
+
+const WarningText = styled.p`
+  padding: 0;
+
+  color: #a00606;
+  font-weight: 600;
+  font-size: 0.7em;
 `;
